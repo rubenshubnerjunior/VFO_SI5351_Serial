479915b3196c6b16ba95a7531c4795ca00f88271
rubenshubnerjunior <126208027+rubenshubnerjunior@users.noreply.github.com>
2024-01-02 15:48:50 -0300

    desenvolvendo tela serial

--- comando.py
+++ comando.py
@@ -3,38 +3,38 @@
 from tkinter import *
 janela = Tk()
 
-def quadrado (x):
-    quadrado = x ** 2
-    return quadrado
 
 
-def quadrado_e_cubo (x):
-    quadrado = x ** 2
-    cubo = x ** 3
-    return quadrado, cubo
+def concatenar(): # Concatena valores digitatos nos botoes
 
 
-print(quadrado(5))
-print(quadrado_e_cubo(6))
+
+    return
 
 
-janela.title('Primeira janela')
+janela.title('Ajusta VFO')
 
 
-bt0 = Button(janela, text = '0',height= 4, width=5) 
-bt1 = Button(janela, text = '1',height= 4, width=5)
-bt2 = Button(janela, text = '2',height= 4, width=5)
-bt3 = Button(janela, text = '3',height= 4, width=5)
-bt4 = Button(janela, text = '4',height= 4, width=5)
-bt5 = Button(janela, text = '5',height= 4, width=5)
-bt6 = Button(janela, text = '6',height= 4, width=5)
-bt7 = Button(janela, text = '7',height= 4, width=5)
-bt8 = Button(janela, text = '8',height= 4, width=5)
-bt9 = Button(janela, text = '9',height= 4, width=5)
+bt0 =    Button(janela, text = '0',height= 4, width=5) 
+bt1 =    Button(janela, text = '1',height= 4, width=5)
+bt2 =    Button(janela, text = '2',height= 4, width=5)
+bt3 =    Button(janela, text = '3',height= 4, width=5)
+bt4 =    Button(janela, text = '4',height= 4, width=5)
+bt5 =    Button(janela, text = '5',height= 4, width=5)
+bt6 =    Button(janela, text = '6',height= 4, width=5)
+bt7 =    Button(janela, text = '7',height= 4, width=5)
+bt8 =    Button(janela, text = '8',height= 4, width=5)
+bt9 =    Button(janela, text = '9',height= 4, width=5)
 btVirg = Button(janela, text = ',',height= 4, width=5)
-btHz = Button(janela, text = 'Hz',height= 4, width=5)
-btKhz = Button(janela, text = 'Khz',height= 4, width=5)
-btMhz = Button(janela, text = 'Mhz',height= 4, width=5)
+btHz =   Button(janela, text = 'Hz',height= 4, width=5)
+btKhz =  Button(janela, text = 'Khz',height= 4, width=5)
+btMhz =  Button(janela, text = 'Mhz',height= 4, width=5)
+
+labelDig = Label(janela, text="Digitado:")
+inDig = Entry(janela,width=30)
+
+labelPorta = Label(janela, text="Porta Serial:")
+portaSerial = Entry(janela,width=30)
 
 
 bt7.grid(row = 0, column = 0, sticky = W, pady = 5)
@@ -56,9 +56,18 @@ bt0.grid(row = 3, column = 0, sticky = W, pady = 5)
 btVirg.grid(row = 3, column = 1, sticky = W, pady = 5)
 
 
+labelDig.grid(row = 4, column = 0, sticky = W, pady = 5)
+inDig.grid(row = 4, column = 1, columnspan=100)
+
+labelPorta.grid(row = 5, column = 0, sticky = W, pady = 5)
+portaSerial.grid(row = 5, column = 1, columnspan=100)
 
 
-janela.geometry("400x400")
+
+
+
+
+janela.geometry("270x400")
 janela.mainloop()
 
 
