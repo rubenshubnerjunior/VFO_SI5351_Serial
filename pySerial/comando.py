82b3dc969e83844c7c9e77c55901c12456147fa2
rubenshubnerjunior <126208027+rubenshubnerjunior@users.noreply.github.com>
2024-01-04 14:17:21 -0300

    ajustando a GUI

--- pySerial/comando.py
+++ pySerial/comando.py
@@ -2,6 +2,10 @@
 
 # Ainda nao estah terminado
 
+# C:\ python -m pip install pyserial
+
+import serial
+import time
 from tkinter import *
 janela = Tk()
 
@@ -20,22 +24,34 @@ def hz():
     valorDig= inDig.get() #Leitura do campo texto
     fHz=float(valorDig) # conversao para float
     intHz= int(fHz)  # desconsidera a casa decimal
-    print("Hz:",intHz)
+    serialCom("FREQ,"+str(intHz))
 
 def khz():
     valorDig= inDig.get() #Leitura do campo texto
     fKHz=float(valorDig) # conversao para float
     fKHz=fKHz*1000 # escala Khz para Hz
     intKhz=int(fKHz) # desconsidera a casa decimal
-    print("Hz:",intKhz)
+    serialCom("FREQ,"+str(intKhz))
 
 def mhz():
     valorDig= inDig.get() #Leitura do campo texto
     fMHz=float(valorDig) # conversao para float
     fMHz=fMHz*1000000 # escala Mhz para Hz
     intMhz=int(fMHz) # desconsidera a casa decimal
-    print("Hz:",intMhz)
-   
+    serialCom("FREQ,"+str(intMhz))
+
+def serialCom(msg):
+    print(msg)
+    porta= portaSerial.get()
+    #SerialObj = serial.Serial(porta)
+    #SerialObj.baudrate = 115200 # set Baud rate to 115200
+    #SerialObj.bytesize = 8   # Number of data bits = 8
+    #SerialObj.parity  ='N'   # No parity
+    #SerialObj.stopbits = 1   # Number of Stop bits = 1
+    #time.sleep(3)
+
+    #SerialObj.close()      # Close the port
+
 
 janela.title('Ajusta VFO SI5351')
 
@@ -43,21 +59,21 @@ fonte="Helvetica 10 bold"
 
 #=======================Elementos da GUI=========================================
 
-bt0 =       Button(janela,font=fonte,padx=50,pady=20, text = '0', command=lambda:concatenar("0")) # lambda para passar paramentro para a funcao
-bt1 =       Button(janela,font=fonte,padx=50,pady=20, text = '1', command=lambda:concatenar("1")) # lambda para passar paramentro para a funcao
-bt2 =       Button(janela,font=fonte,padx=50,pady=20, text = '2', command=lambda:concatenar("2")) # lambda para passar paramentro para a funcao
-bt3 =       Button(janela,font=fonte,padx=50,pady=20, text = '3', command=lambda:concatenar("3")) # lambda para passar paramentro para a funcao
-bt4 =       Button(janela,font=fonte,padx=50,pady=20, text = '4', command=lambda:concatenar("4")) # lambda para passar paramentro para a funcao
-bt5 =       Button(janela,font=fonte,padx=50,pady=20, text = '5', command=lambda:concatenar("5")) # lambda para passar paramentro para a funcao
-bt6 =       Button(janela,font=fonte,padx=50,pady=20, text = '6', command=lambda:concatenar("6")) # lambda para passar paramentro para a funcao
-bt7 =       Button(janela,font=fonte,padx=50,pady=20, text = '7', command=lambda:concatenar("7")) # lambda para passar paramentro para a funcao
-bt8 =       Button(janela,font=fonte,padx=50,pady=20, text = '8', command=lambda:concatenar("8")) # lambda para passar paramentro para a funcao
-bt9 =       Button(janela,font=fonte,padx=50,pady=20, text = '9', command=lambda:concatenar("9")) # lambda para passar paramentro para a funcao
-btVirg =    Button(janela,font=fonte,padx=50,pady=20, text = ',', command=lambda:concatenar(".")) # lambda para passar paramentro para a funcao
-btHz =      Button(janela,font=fonte,padx=50,pady=20, text = 'Hz',command = hz) # chamada da funcao sem passar paramentro
-btKhz =     Button(janela,font=fonte,padx=50,pady=20, text = 'Khz',command = khz) # chamada da funcao sem passar paramentro
-btMhz =     Button(janela,font=fonte,padx=50,pady=20, text = 'Mhz',command = mhz) # chamada da funcao sem passar paramentro
-btLimpar =  Button(janela,font=fonte,padx=50,pady=20, text = 'DEL', command = limpar) # chamada da funcao sem passar paramentro
+bt0 =       Button(janela,font=fonte,padx=50,pady=20, text = '0',relief="raised", command=lambda:concatenar("0")) # lambda para passar paramentro para a funcao
+bt1 =       Button(janela,font=fonte,padx=50,pady=20, text = '1',relief="raised", command=lambda:concatenar("1")) # lambda para passar paramentro para a funcao
+bt2 =       Button(janela,font=fonte,padx=50,pady=20, text = '2',relief="raised", command=lambda:concatenar("2")) # lambda para passar paramentro para a funcao
+bt3 =       Button(janela,font=fonte,padx=50,pady=20, text = '3',relief="raised", command=lambda:concatenar("3")) # lambda para passar paramentro para a funcao
+bt4 =       Button(janela,font=fonte,padx=50,pady=20, text = '4',relief="raised", command=lambda:concatenar("4")) # lambda para passar paramentro para a funcao
+bt5 =       Button(janela,font=fonte,padx=50,pady=20, text = '5',relief="raised", command=lambda:concatenar("5")) # lambda para passar paramentro para a funcao
+bt6 =       Button(janela,font=fonte,padx=50,pady=20, text = '6',relief="raised", command=lambda:concatenar("6")) # lambda para passar paramentro para a funcao
+bt7 =       Button(janela,font=fonte,padx=50,pady=20, text = '7',relief="raised", command=lambda:concatenar("7")) # lambda para passar paramentro para a funcao
+bt8 =       Button(janela,font=fonte,padx=50,pady=20, text = '8',relief="raised", command=lambda:concatenar("8")) # lambda para passar paramentro para a funcao
+bt9 =       Button(janela,font=fonte,padx=50,pady=20, text = '9',relief="raised", command=lambda:concatenar("9")) # lambda para passar paramentro para a funcao
+btVirg =    Button(janela,font=fonte,padx=50,pady=20, text = ',',relief="raised", command=lambda:concatenar(".")) # lambda para passar paramentro para a funcao
+btHz =      Button(janela,font=fonte,padx=45,pady=20, text = 'Hz',relief="raised",command = hz) # chamada da funcao sem passar paramentro
+btKhz =     Button(janela,font=fonte,padx=40,pady=20, text = 'Khz',relief="raised",command = khz) # chamada da funcao sem passar paramentro
+btMhz =     Button(janela,font=fonte,padx=40,pady=20, text = 'Mhz',relief="raised",command = mhz) # chamada da funcao sem passar paramentro
+btLimpar =  Button(janela,font=fonte,padx=40,pady=20, text = 'DEL',relief="raised", command = limpar) # chamada da funcao sem passar paramentro
 
 labelDig = Label(janela, font=fonte, text="Digitado:")
 inDig = Entry(janela,font=fonte,width=30)
@@ -65,7 +81,7 @@ inDig = Entry(janela,font=fonte,width=30)
 labelPorta = Label(janela,font=fonte, text="Porta Serial:")
 portaSerial = Entry(janela,font=fonte,width=30)
 
-portaSerial.insert(0,"COM 1")
+portaSerial.insert(0,"COM1")
 
 
 #=============Colocando componentes na grid===============================
@@ -86,17 +102,17 @@ btMhz.grid(row = 2, column = 3)
 
 bt0.grid(row = 3, column = 0)
 btVirg.grid(row = 3, column = 1)  # linha 3
-btLimpar.grid(row = 3, column =3)
+btLimpar.grid(row = 3, column =2)
 
 
-labelDig.grid(row = 4, column = 0)
-inDig.grid(row = 4,    column = 1)  # linha 4
+labelDig.grid(row = 5, column = 0)
+inDig.grid(row = 5,    column = 1)  # linha 4
 
-labelPorta.grid(row = 5, column = 0)
-portaSerial.grid(row = 5, column = 1)  # linha 5
+labelPorta.grid(row = 6, column = 0)
+portaSerial.grid(row = 6, column = 1)  # linha 5
 
-
-janela.geometry("630x400")
+janela.configure(bg='WHITE')
+janela.geometry("580x400")
+janela.resizable(False, False)
 janela.mainloop()
 
-janela.mainloop() 
